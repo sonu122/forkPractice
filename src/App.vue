d6af19ca76620429b99de998c84c195ff21246da
Vinod <vinod.yadav@vidyamantra.com>
2022-04-19 03:30:37 -0400

    added tailwind

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,14 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <button class="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+  Save changes
+</button>
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
